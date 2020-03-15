7f41ac4cb1aaeacb206fe0d338cc506718d40b4e
Brarou <52816@etu.he2b.be>
2020-03-15 10:45:38 +0100

    Implementation of EntityType and SquareType

---
+++ src/esi/g52816/model/EntityType.java
@@ -0,0 +1,10 @@
+package esi.g52816.model;
+
+/**
+ *
+ * @author bilal
+ */
+public enum EntityType {
+    //REPRESENT A ENTITY BY A BOX,A PLAYER OR VOID
+    BOX,PLAYER,VOID;
+}
---
+++ src/esi/g52816/model/SquareType.java
@@ -0,0 +1,10 @@
+package esi.g52816.model;
+
+/**
+ *
+ * @author bilal
+ */
+public enum SquareType {
+    //REPRESENT A SQUARE BY WALL,GROUND OR STORAGE
+    GROUND,WALL,STORAGE;
+}
